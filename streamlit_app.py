8c15ee8beca45f2f788da5b9db41511f20fe2ecb
Carol Luca Gasan <gasancarolluca@gmail.com>
2023-03-26 11:20:50 +0300

    Simple version of the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 # here is going to be the implementation
 # of the deployed app - for now the file
 # will be empty for streamlit's setup !!
+
+import streamlit as st
+from scraper import get_heuristics
+
+def my_app():
+    url = st.text_input('Enter URL')
+    if url:
+        st.write(get_heuristics(url))
+
+if __name__ == "__main__":
+    my_app()
